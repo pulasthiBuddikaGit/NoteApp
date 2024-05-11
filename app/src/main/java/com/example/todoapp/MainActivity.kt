c83237ed011724d63240c16a987645cfd27fa4e7
Pulasthi Buddika <151649555+pulasthiBuddikaGit@users.noreply.github.com>
2024-05-11 17:35:14 +0530

    layouts

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.Addbutton.setOnClickListener{
+            val intent = Intent(this,AddNoteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
